ac6e837cebf421edac08b423747e9d4974edf690
Daniel Williams <dw@daniels-mbp-2.home>
2019-11-16 22:41:13 +0000

    Remove development creation of fake data

--- server/models/index.js
+++ server/models/index.js
@@ -4,13 +4,17 @@ require('dotenv').config();
 import User from './user';
 import Message from './message';
 
-const url = 'mongodb://admin_dan:danonly06@ds061611.mlab.com:61611/heroku_pzk57mmp'
+const url =
+  'mongodb://admin_dan:danonly06@ds061611.mlab.com:61611/heroku_pzk57mmp';
 
 // const url = process.env.DB_URL
-console.log(url)
+console.log(url);
 
 const connectDb = () => {
-  return mongoose.connect(url, { useNewUrlParser: true});
+  return mongoose.connect(url, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
 };
 
 const models = { User, Message };
