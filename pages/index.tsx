9b4b1e8b18871422ae4bb396e2d59ad5121c4870
Daniel Williams <dw@daniels-mbp-2.home>
2019-11-18 19:57:34 +0000

    Replace port with env var

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,12 @@ const Index = () => (
 );
 
 Index.getInitialProps = async function() {
-  const res = await fetch('http://localhost:3000/api/a');
+  const port: any = process.env.PORT || 3000;
+  const res = await fetch(`http://localhost:${port}/api/a`);
   const data = await res.json();
-  console.log(data.a)
+  console.log(data.a);
   console.log(`Show data fetched. Count: ${data}`);
-  return ({data})
+  return { data };
 };
 
 export default Index;
