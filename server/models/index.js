eadeac70c62639fbb192003155e5ed731dbdfa76
Daniel Williams <dw@daniels-mbp-2.home>
2020-02-15 21:06:58 +0000

    Change server retrival of key

--- server/models/index.js
+++ server/models/index.js
@@ -4,7 +4,11 @@ require('dotenv').config();
 import User from './user';
 import Message from './message';
 
-const url = process.env.DB_URL;
+// const url =
+//   'mongodb://admin_dan:danonly06@ds061611.mlab.com:61611/heroku_pzk57mmp';
+
+const url = process.env.MONGODB_URI;
+
 const connectDb = () => {
   return mongoose.connect(url, {
     useUnifiedTopology: true,
